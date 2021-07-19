039f5e3464948532ca0cf60bcb098e66915cd30c
Huu Bao Nguyen <huubao2309@gmail.com>
2021-07-19 20:53:44 +0700

    add

---
+++ android/app/src/main/kotlin/com/custom_paint/practice_custom_paint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.custom_paint.practice_custom_paint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'paint_vietnam_flag/paint_vietnam_flag.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: VietnamFlag(),
+          ),
+        ),
+      ),
+    );
+  }
+}
